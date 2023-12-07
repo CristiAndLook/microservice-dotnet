793d35294db00f48454693be610e79b67be751c1
CristiAndLook <krhistian1902@gmail.com>
2023-12-07 01:11:52 -0500

    API blobs

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using micronet.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.services.AddTransient<IBlob blob>();
+builder.Services.AddTransient<IBlob, Blob>();
 
 builder.Services.AddDbContext<DataContext>(options =>
 {
@@ -32,29 +32,40 @@ app.MapGet("/adults/{id}", async (DataContext context, int id) => await context.
 .WithName("GetAdultById")
 .WithOpenApi();
 
-app.MapPost("/adults/add", async (DataContext context, Adult adult) =>
+app.MapPost("members/add", async (IFormFile file, DataContext context, IBlob blob, string name, string LastName, int birthyear) =>
 {
-    context.Adults.Add(adult);
-    await context.SaveChangesAsync();
-    return Results.Ok(await GetAdults(context));
-})
-.WithName("AddAdult")
-.WithOpenApi();
+    string fileName = string.Format("{0}{1}.jpg", name.ToLower(), LastName.ToLower());
 
-app.MapPost("/members/add", async (IFormFile file, DataContext context, IBlob blob, string name, string lastname, int birthyear) =>
-{
-    string filename = string.Format("{0}{1}.jpg", name.ToLower(), lastname.ToLower());
-    if(birthyear <= 17) {
-        var item = new Child() { Name = name, LastName = lastname, BirthYear = birthyear, ImageUrl = filename };
+    if (birthyear <= 17)
+    {
+        var item = new Child
+        {
+            Name = name,
+            LastName = LastName,
+            BirthYear = birthyear,
+            ImageUrl = fileName
+        };
         context.Children.Add(item);
     }
-    else {
-        var item = new Adult() { Name = name, LastName = lastname, BirthYear = birthyear, ImageUrl = filename };
+    else
+    {
+        var item = new Adult
+        {
+            Name = name,
+            LastName = LastName,
+            BirthYear = birthyear,
+            ImageUrl = fileName
+        };
         context.Adults.Add(item);
     }
+
+    await context.SaveChangesAsync();   
+    await blob.Upload(file, fileName);
+    return Results.Ok();
+
+
 })
-.WithName("AddMember")
-.WithOpenApi();
+.WithName("AddMember");
 
 
 app.Run();
